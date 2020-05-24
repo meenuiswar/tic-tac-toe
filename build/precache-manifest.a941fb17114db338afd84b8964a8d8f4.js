597cd11966b33957b9b58b7bd7979782e04e6072
Meenu Iswar <iswarmeenu@gmail.com>
2020-05-24 11:13:01 +0530

    trial

---
+++ build/precache-manifest.a941fb17114db338afd84b8964a8d8f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8fe3594721d769424f2129565988fba",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "047969409913a23f7801",
+    "url": "/tic-tac-toe/static/css/main.d10940ec.chunk.css"
+  },
+  {
+    "revision": "2619b92737e4ff9c1ad2",
+    "url": "/tic-tac-toe/static/js/2.9af09195.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tic-tac-toe/static/js/2.9af09195.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "047969409913a23f7801",
+    "url": "/tic-tac-toe/static/js/main.b842c83c.chunk.js"
+  },
+  {
+    "revision": "9c49f2d6e5cec98919fe",
+    "url": "/tic-tac-toe/static/js/runtime-main.f72e9f27.js"
+  }
+]);
